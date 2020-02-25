32cb73fde39bd5a4d4dcdd53b55537ba2ff11c1e
rerueter <rerueter@gmail.com>
2020-02-24 16:35:04 -0800

    HEROKU

--- adventure_buddy/settings.py
+++ adventure_buddy/settings.py
@@ -132,6 +132,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static')),
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
